9fbfcbaf7ff40c6dd359c8ae9000f2a87f9ed1fa
vibe as <theresegugle@gmail.com>
2026-01-21 16:36:54 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
-﻿self.addEventListener('fetch', () => {});
+﻿self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("vibe-cache").then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ])
+    )
+  );
+});
+
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
